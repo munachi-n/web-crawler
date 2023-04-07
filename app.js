502853707222683bc304dcbb34a4936eaad0da85
munachi-n <munachi-n@users.noreply.github.com>
2023-04-07 12:45:00 +0100

    Wire up rate limiter and performance monitor in app.js

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ const path = require('path');
 const config = require('./config');
 const routes = require('./routes');
 const { errorHandler, notFoundHandler } = require('./middlewares/errorHandler');
+const RateLimiter = require('./middlewares/rateLimiter');
+const PerformanceMonitor = require('./middlewares/performanceMonitor');
 
 const app = express();
 
-// Middleware
+// Performance monitoring
+const perfMonitor = new PerformanceMonitor();
+app.use(perfMonitor.middleware());
+
+// Rate limiting
+const limiter = new RateLimiter({
+  windowMs: config.rateLimit.windowMs,
+  maxRequests: config.rateLimit.maxRequests,
+});
+app.use('/api', limiter.middleware());
+
+// Standard middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
@@ -25,5 +38,8 @@ app.use('/', routes);
 // Error handling
 app.use(notFoundHandler);
 app.use(errorHandler);
+
+// Expose for status endpoint
+app.locals.perfMonitor = perfMonitor;
 
 module.exports = app;
