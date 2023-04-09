812af6b5e1a8ced21e8fba2dce3726b0011a003d
munachi-n <munachi-n@users.noreply.github.com>
2023-04-09 10:10:00 +0100

    Integrate config validation and graceful shutdown into server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const app = require('./app');
 const config = require('./config');
+const GracefulShutdown = require('./utils/shutdown');
+const ConfigValidator = require('./config/validator');
+
+// Validate config on startup
+const validation = ConfigValidator.validate(config);
+if (!validation.valid) {
+  console.error('Config errors:', validation.errors);
+  process.exit(1);
+}
 
 const PORT = config.port;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Web Crawler server running on http://localhost:${PORT}`);
+  console.log(`Environment: ${config.nodeEnv}`);
 });
+
+// Graceful shutdown
+const shutdown = new GracefulShutdown();
+shutdown
+  .register('Close HTTP server', () => new Promise((resolve) => server.close(resolve)))
+  .listen(server);
